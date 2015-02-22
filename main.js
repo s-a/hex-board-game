0c359e101680378f41ffabede6ca1ade71861cc4
s-a <stephan.ahlf@googlemail.com>
2015-02-22 14:52:26 +0100

    refresh tiles on resize

--- main.js
+++ main.js
@@ -3,23 +3,24 @@
 
     var $board = $("#game-board");
     var board = $board.get(0);
-    var colCount = 22;
+    var x;
     board.addEventListener('scroll', function (event) {
         var trueDivHeight = board.scrollHeight;
         var divHeight = $board.height();
         var maxScollTop = trueDivHeight - divHeight; 
         var bottomHex = $(".hex:first");
         var rowsToAdd = 10;
+        var currentColCount = game.GUI.grid.state.rows[0].length;
         if (board.scrollTop === maxScollTop){
             board.scrollTop -= bottomHex.height() * (rowsToAdd - 1);
             for (var i = 0; i < rowsToAdd; i++) {
-                addRow(colCount, true);
+                addRow(currentColCount, true);
             }
         }
         game.GUI.grid.setState({rows: game.GUI.grid.state.rows}); 
     });
 
-    var x = 0;
+    
     var addRow = function  (cols, magic, done) {
         var rows = game.GUI.grid.state.rows;
          if (magic){
@@ -34,34 +35,38 @@
             }
           };
           row.push(new Field(fieldParms));
-        };
+        }
         rows.push(row);
         x++;
     };
 
 
-    $("body").on("game-components-ready", function  () {
-        for (var i = 0; i < 22; i++) {
-            addRow(colCount);
-        };
-        game.style.setCss(1);
-        game.GUI.grid.setState({rows: game.GUI.grid.state.rows}); 
-    });
 
-    $("#game-board").one('transitionend', ".hex", function(e) {
-        var board = document.getElementById("game-board"); 
-        var touchDevice = ('ontouchstart' in window);
-        if (!touchDevice){
-            window.setTimeout(function() {
-                $('#game-board').overscroll({
-                    showThumbs : false
-                });
-            }, 800);
-         }
-    });
-  
-  $(function() {
 
-  });
+    var load = function  () {
+        x = 0;
+        game.GUI.grid.state.rows = [];
+        var needed = game.GUI.board.neededHexTilesToOverfillScreen();
+        for (var i = 0; i < needed.rowCount; i++) {
+            addRow(needed.colCount);
+        }
+
+        game.GUI.board.zoom(2);
+
+        $board.one('transitionend', ".hex", function(e) {
+            board.scrollTop = board.scrollHeight/4;
+            board.scrollLeft = board.scrollWidth/4;
+        });
+
+        game.GUI.grid.setState({rows: game.GUI.grid.state.rows}, function  () {
+            board.scrollTop = board.scrollHeight/4;
+            board.scrollLeft = board.scrollWidth/4;
+        }); 
+    };
+
+    $("body").on("game-components-ready", load);
+    $(window).resize(load);
+
+   
 
 })(window.jQuery, window.game, window.game.classes.Field);
